c614da36dd28fd0f5cbf310cf04f33362d0117fe
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2020-12-05 07:55:40 +0500

    AddTransection

--- ems_app/src/App.js
+++ ems_app/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import {Header} from './components/Header'
 import {Balance} from './components/Balance'
+import {IncomeExpence} from './components/IncomeExpence'
+import {TransectionList} from './components/TransectionList'
+import {AddTransection} from './components/AddTransection'
+
 function App() {
   return (
     <div>
       <Header/>
       <div className="container">
         <Balance/>
+        <IncomeExpence/>
+        <TransectionList/>
+        <AddTransection/>
 
       </div>
           </div>
