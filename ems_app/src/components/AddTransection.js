8e349deb8645c5783b6f71b9b0e0c9c5deb34a1a
Engineer Qurban Ali <42018848+QurbanGujjar@users.noreply.github.com>
2020-12-05 08:53:02 +0500

    update all

--- ems_app/src/components/AddTransection.js
+++ ems_app/src/components/AddTransection.js
@@ -1,18 +1,18 @@
 import React,{useState} from 'react'
 
 export const AddTransection = () => {
-    const [taxt,setText]=useState('');
+    const [text,setText]=useState('');
     const [amount,setAmount]=useState(0)
     return (
         <div>
 <h3>Add new transaction</h3>
       <form >
         <div className="form-control">
-          <label htmlfor="text">Text</label>
+          <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e)=> setText(e.target.value) } placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label htmlfor="amount"
+          <label htmlFor="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
